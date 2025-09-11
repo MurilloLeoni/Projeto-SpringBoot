cabf1887c0a1df5f9bf41d96e02d7d8e0779cf9f
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-11 19:16:16 -0300

    Exception handling - findById

---
+++ src/main/java/com/murilloleoni/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.murilloleoni.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
