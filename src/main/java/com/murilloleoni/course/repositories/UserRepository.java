98a31a79a9f09aa1d81d5df36c457e5c22d4fb6a
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-11 10:52:40 -0300

    Service layer, component registration

--- src/main/java/com/murilloleoni/course/repositories/UserRepository.java
+++ src/main/java/com/murilloleoni/course/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.murilloleoni.course.repositories;
 import com.murilloleoni.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
